6f6ba06ef4f20c4f6141aeda44aedafe162f939b
Uli3000 <ulisesgamer281@gmail.com>
2025-04-09 16:55:49 -0600

    Create the interface to see the products

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,35 @@
-export default function OrderPage({params} : {params: { category: string }}) {
-  console.log(params.category)
+import ProductCard from "@/components/products/ProductCard";
+import { prisma } from "@/src/lib/prisma"
+
+async function getProducts(category: string){
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category
+      }
+    }
+  })
+
+  return products;
+}
+
+export default async function OrderPage({params} : {params: { category: string }}) {
+  const products = await getProducts(params.category)
+
   return (
-    <div>OrderPage</div>
+    <>
+    <h1 className="text-2xl my-10">
+      Elige y personaliza tu pedido a continuacion
+    </h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 items-start gap-4">
+        {products.map(product =>(
+          <ProductCard 
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+    </>
   )
 }
---
+++ src/utils/indedx.ts
@@ -0,0 +1,6 @@
+export function formatCurrency(amout: number){
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(amout);
+}
