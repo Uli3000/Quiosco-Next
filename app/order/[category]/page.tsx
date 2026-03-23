1a896351abeae76838ebad5ffd794ab4c77a9fe9
Uli3000 <ulisesgamer281@gmail.com>
2026-03-22 22:48:16 -0600

    fix again

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,9 +1,8 @@
 import ProductCard from "@/components/products/ProductCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma"
-import { Product } from "@prisma/client";
 
-async function getProducts(category: string): Promise<Product[]>{
+async function getProducts(category: string){
   const products = await prisma.product.findMany({
     where: {
       category: {
