98cc604ac857edc453bcab54e0db231ae41f921e
Uli3000 <ulisesgamer281@gmail.com>
2026-03-22 23:46:53 -0600

    fix orderSidebar type map

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -2,25 +2,27 @@ import { prisma } from '@/src/lib/prisma'
 import CategoryIcon from '../ui/CategoryIcon'
 import Logo from '../ui/Logo'
 
-async function getCategories(){
-  return  await prisma.category.findMany()
+// Infiere el tipo de retorno de findMany
+type Category = Awaited<ReturnType<typeof prisma.category.findMany>>[number]
+
+async function getCategories(): Promise<Category[]> {
+  return await prisma.category.findMany()
 }
 
 export default async function OrderSidebar() {
-
   const categories = await getCategories()
 
   return (
     <aside className="md:w-72 md:h-screen bg-white">
       <Logo />
-        <nav className='mt-10'>
-          {categories.map((category) =>(
-            <CategoryIcon 
-              key={category.id}
-              category={category}
-            />
-          ))}
-        </nav>
+      <nav className='mt-10'>
+        {categories.map((category: Category) => (
+          <CategoryIcon 
+            key={category.id}
+            category={category}
+          />
+        ))}
+      </nav>
     </aside>
   )
 }
