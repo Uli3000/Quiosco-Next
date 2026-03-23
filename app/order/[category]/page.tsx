ee28d33346c16918d1ce43b0d0f657df831ab379
Uli3000 <ulisesgamer281@gmail.com>
2026-03-22 23:07:34 -0600

    now yes

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -14,14 +14,15 @@ async function getProducts(category: string){
   return products;
 }
 
-export default async function OrderPage({params} : {params: { category: string }}) {
-  const products = await getProducts(params.category)
+export default async function OrderPage({params} : {params: Promise<{ category: string }>}) {
+  const { category } = await params
+  const products = await getProducts(category)
 
   return (
     <>
-    <Heading>
-      Elige y personaliza tu pedido a continuacion
-    </Heading>
+      <Heading>
+        Elige y personaliza tu pedido a continuacion
+      </Heading>
 
       <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 items-start gap-4">
         {products.map(product =>(
