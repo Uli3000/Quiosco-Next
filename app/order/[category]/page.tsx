6699dd2667083b9c127a409a2a2ecf89645ab078
Uli3000 <ulisesgamer281@gmail.com>
2026-03-22 23:05:20 -0600

    prueba

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -14,10 +14,8 @@ async function getProducts(category: string){
   return products;
 }
 
-export default async function OrderPage({searchParams} : {searchParams: Promise<{ [key: string]: string | string[] | undefined }>;}) {
-  const { category } = await searchParams
-  const categoryParam = typeof category === "string" ? category : ""
-  const products = await getProducts(categoryParam)
+export default async function OrderPage({params} : {params: { category: string }}) {
+  const products = await getProducts(params.category)
 
   return (
     <>
