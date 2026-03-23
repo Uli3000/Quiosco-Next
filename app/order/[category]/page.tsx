def0b1a472682a18f1fb582861c1d72426ab00da
Uli3000 <ulisesgamer281@gmail.com>
2026-03-22 23:10:52 -0600

    and again

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -2,7 +2,10 @@ import ProductCard from "@/components/products/ProductCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma"
 
-async function getProducts(category: string){
+// Infiere el tipo de retorno de findMany
+type Product = Awaited<ReturnType<typeof prisma.product.findMany>>[number]
+
+async function getProducts(category: string): Promise<Product[]> {
   const products = await prisma.product.findMany({
     where: {
       category: {
@@ -14,9 +17,9 @@ async function getProducts(category: string){
   return products;
 }
 
-export default async function OrderPage({params} : {params: Promise<{ category: string }>}) {
+export default async function OrderPage({params}: {params: Promise<{ category: string }>}) {
   const { category } = await params
-  const products = await getProducts(category)
+  const products: Product[] = await getProducts(category)
 
   return (
     <>
@@ -25,7 +28,7 @@ export default async function OrderPage({params} : {params: Promise<{ category: 
       </Heading>
 
       <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 items-start gap-4">
-        {products.map(product =>(
+        {products.map((product: Product) => (
           <ProductCard 
             key={product.id}
             product={product}
